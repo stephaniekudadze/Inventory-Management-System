35b1ea354cb8279d8f4fa9ecafc656f4bc7cc031
Stephanie Kudadze <Stephaniekd72@gmail.com>
2023-07-20 03:35:37 -0700

    Project Progress

--- src/main/java/com/example/inventorymanager/HelloController.java
+++ src/main/java/com/example/inventorymanager/HelloController.java
@@ -1,5 +1,4 @@
 package com.example.inventorymanager;
-
 import java.util.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
@@ -8,16 +7,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 
 public class HelloController {
-
-    private Stack<String> goodsStack;
-    private HashMap<String, Integer> vendorGoodsCountMap;
-    private Queue<String> issuedGoodsQueue;
-    private List<String> vendorsList;
-
-
 
     private Stage stage;
     private Scene scene;
@@ -58,53 +51,30 @@ public class HelloController {
         stage.show();
 
     }
-
-    public void InventoryController() {
-        goodsStack = new Stack<>();
-        vendorGoodsCountMap = new HashMap<>();
-        issuedGoodsQueue = new LinkedList<>();
-        vendorsList = new ArrayList<>();
+    public void switchToViewGoods(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("ViewGoods.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
-    // View Vendors
-    public List<String> getViewVendors() {
-        return vendorsList;
+    public void switchToViewBills(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("ViewBills.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
-    // Add Goods
-    public void addGoods(String goods) {
-        goodsStack.push(goods);
-
-        // Update vendor-goods count
-        vendorGoodsCountMap.put(goods, vendorGoodsCountMap.getOrDefault(goods, 0) + 1);
+    public void switchToViewIssuedGoods(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("ViewIssuedGoods.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
-    // Issue Goods
-    public String issueGoods() {
-        if (!goodsStack.isEmpty()) {
-            String goods = goodsStack.pop();
 
-            // Add issued goods to the queue
-            issuedGoodsQueue.add(goods);
-
-            // Update vendor-goods count
-            vendorGoodsCountMap.put(goods, vendorGoodsCountMap.get(goods) - 1);
-
-            return goods;
-        }
-        return null;
-    }
-
-    // Search Goods
-    public boolean searchGoods(String goods) {
-        return goodsStack.contains(goods);
-    }
-
-    // Sort Goods
-    public List<String> sortGoods() {
-        List<String> sortedGoodsList = new ArrayList<>(goodsStack);
-        Collections.sort(sortedGoodsList);
-        return sortedGoodsList;
-    }
 }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.inventorymanager {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.inventorymanager to javafx.fxml;
